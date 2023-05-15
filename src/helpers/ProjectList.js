ecc0b01d023111c210086480824d3730ad224644
priyank <patelpriyank3112002@gmail.com>
2023-05-16 00:17:36 +0530

    ..

--- src/helpers/ProjectList.js
+++ src/helpers/ProjectList.js
@@ -1,8 +1,9 @@
 import Proj1 from "../assets/blog-project.png";
-import Proj2 from "../assets/text-utils.png";
-import Proj3 from "../assets/tic-tac-toe.png";
-import Proj4 from "../assets/sticky-notes.png";
-import Proj5 from "../assets/food-delivery.png";
+import Proj2 from "../assets/youtube_clone.png";
+import Proj3 from "../assets/text-utils.png";
+import Proj4 from "../assets/tic-tac-toe.png";
+import Proj5 from "../assets/sticky-notes.png";
+import Proj6 from "../assets/food-delivery.png";
 
 
 export const ProjectList = [
@@ -12,23 +13,28 @@ export const ProjectList = [
     skills: "Mongodb,Express.js,React.js,Node.js"
   },
   {
-    name: "TextUtils",
+    name: "You_tube clone",
     image: Proj2,
+    skills: "React.js,Material-Ui,Rapid API"
+  },
+  {
+    name: "TextUtils",
+    image: Proj3,
     skills: "React,bootstrap",
   },
   {
     name: "Tic_Tac_toe",
-    image: Proj3,
-    skills: "HTML,CSS,Javascript",
-  },
-  {
-    name: "Sticky-Notes",
     image: Proj4,
     skills: "HTML,CSS,Javascript",
   },
   {
-    name: "Responsive-UI",
+    name: "Sticky-Notes",
     image: Proj5,
+    skills: "HTML,CSS,Javascript",
+  },
+  {
+    name: "Responsive-UI",
+    image: Proj6,
     skills: "JavaScript,HTML,CSS",
   }
  
