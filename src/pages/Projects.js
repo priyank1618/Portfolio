cf7058664c6d82208c508608f561f7c36d7e1a95
priyank <patelpriyank3112002@gmail.com>
2023-05-15 22:48:07 +0530

    ...

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -8,11 +8,11 @@ import ProjectItem from "../components/ProjectNavigator";
 function Projects() {
   return (
     <div className="projects">
-      <h1> My Personal Projects</h1>
+      <h1 className="projectTitle"> My Personal Projects</h1>
       <div className="projectList">
         {ProjectList.map((project, idx) => {
           return (
-          <ProjectItem id={idx} name={project.name} image={project.image}/>
+          < ProjectItem id={idx} name={project.name} image={project.image}  />
           );
         })}
       </div>
